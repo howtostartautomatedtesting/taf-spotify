2ac910b2837c245ff3e819eddcc2b3a984771e9f
Dzianis_Haidukevich <den.94ew@gmail.com>
2022-08-06 18:21:34 +0300

    Debug

--- src/test/java/by/itacademy/spotify/ui/page/SearchPage.java
+++ src/test/java/by/itacademy/spotify/ui/page/SearchPage.java
@@ -5,33 +5,28 @@ import org.openqa.selenium.support.FindBy;
 
 public class SearchPage extends AuthorizedHomePage {
 
-    String NameArtist = "Eminem";
-    String NameOfSong = "Superman";
+    private static final String NameArtist = "Eminem";
+    private static final String NameOfSong = "Superman";
 
     @FindBy(xpath = "//span[@class='Type__TypeElement-goli3j-0 hWmToD ellipsis-one-line']")
     private WebElement search;
     @FindBy(xpath = "//input[@class='Type__TypeElement-goli3j-0 cPwEdQ QO9loc33XC50mMRUCIvf']")
-    private WebElement clickOnSearch;
-    @FindBy(xpath = "//input[@class='Type__TypeElement-goli3j-0 cPwEdQ QO9loc33XC50mMRUCIvf']")
-    private WebElement inputNameArtist;
-    @FindBy(xpath = "//input[@class='Type__TypeElement-goli3j-0 cPwEdQ QO9loc33XC50mMRUCIvf']")
-    private WebElement inputNameOfSong;
-
+    private WebElement inputTextForSearh;
 
     public String search() {
         search.click();
         return null;
     }
     public String clickOnSearch() {
-        clickOnSearch.click();
+        inputTextForSearh.click();
         return null;
     }
     public String inputNameArtist() {
-        inputNameArtist.getAttribute(NameArtist);
+        inputTextForSearh.getAttribute(NameArtist);
         return null;
     }
     public String inputNameOfSong() {
-        inputNameOfSong.getAttribute(NameOfSong);
+        inputTextForSearh.getAttribute(NameOfSong);
         return null;
     }
 }
