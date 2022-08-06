60f3fd2c69af4ff29265e0bd67c8916ca9d0e771
Alexandr Shinkarev <alexandrshinkarev1610@gmail.com>
2022-08-07 00:57:25 +0300

    fix: locators

--- src/test/java/by/itacademy/spotify/ui/page/PlayListPage.java
+++ src/test/java/by/itacademy/spotify/ui/page/PlayListPage.java
@@ -4,13 +4,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class PlayListPage extends AuthorizedHomePage{
-    @FindBy(xpath = "//button[@aria-label='More options for TestPlayList']")
+    @FindBy(xpath = "//button[@class='T0anrkk_QA4IAQL29get']")
     private WebElement buttonMoreOptionsPlaylist;
-    @FindBy(xpath = "//li[@class='whXv9jYuEgS1DPTmPCe_']")
-    private WebElement linkPlaylistName;
+    @FindBy(xpath = "//button[@class='wCkmVGEQh3je1hrbsFBY']")
+    private WebElement buttonPlaylistName;
 
-    public void clickLinkPlaylistName(){
-        linkPlaylistName.click();
+    public void clickButtonPlaylistName(){
+        buttonPlaylistName.click();
     }
     public void clickButtonMoreOptionsPlaylist(){
         buttonMoreOptionsPlaylist.click();
