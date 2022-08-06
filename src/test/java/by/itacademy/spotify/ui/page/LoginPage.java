7ae08bf7efe7adb0a5b98c617db5944d14cb9d18
Kate Miholenko <katemixa92@gmail.com>
2022-08-06 19:24:39 +0300

    feat: add UI for Login Page

---
+++ src/test/java/by/itacademy/spotify/ui/page/LoginPage.java
@@ -0,0 +1,33 @@
+package by.itacademy.spotify.ui.page;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage{
+    @FindBy(xpath = "//label[@for='login-username']")
+    private WebElement usernameLabel;
+
+    @FindBy(xpath = "//label[@for='login-password']")
+    private WebElement passwordLabel;
+
+    @FindBy(id = "login-username")
+    private WebElement usernameInput;
+
+    @FindBy(id = "login-password")
+    private WebElement passwordInput;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    public void fillUsernameInput(String username) {
+        usernameInput.sendKeys(username);
+    }
+
+    public void fillPasswordInput(String password) {
+        passwordInput.sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        loginButton.click();
+    }
+}
