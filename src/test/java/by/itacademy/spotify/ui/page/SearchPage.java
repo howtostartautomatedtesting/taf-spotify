b16568fc68a3557fd473b94296fbd6fe61c0db89
Dzianis_Haidukevich <den.94ew@gmail.com>
2022-08-06 19:50:35 +0300

    Fix XPath, method

--- src/test/java/by/itacademy/spotify/ui/page/SearchPage.java
+++ src/test/java/by/itacademy/spotify/ui/page/SearchPage.java
@@ -5,28 +5,17 @@ import org.openqa.selenium.support.FindBy;
 
 public class SearchPage extends AuthorizedHomePage {
 
-    private static final String NameArtist = "Eminem";
-    private static final String NameOfSong = "Superman";
-
     @FindBy(xpath = "//span[@class='Type__TypeElement-goli3j-0 hWmToD ellipsis-one-line']")
-    private WebElement search;
-    @FindBy(xpath = "//input[@class='Type__TypeElement-goli3j-0 cPwEdQ QO9loc33XC50mMRUCIvf']")
-    private WebElement inputTextForSearh;
+    private WebElement clickSearch;
+    @FindBy(xpath = "//input[@data-testid='search-input']")
+    private WebElement inputNameArtist;
 
-    public String search() {
-        search.click();
-        return null;
+    public void clickSearch() {
+        clickSearch.click();
     }
-    public String clickOnSearch() {
-        inputTextForSearh.click();
-        return null;
-    }
-    public String inputNameArtist() {
-        inputTextForSearh.getAttribute(NameArtist);
-        return null;
-    }
-    public String inputNameOfSong() {
-        inputTextForSearh.getAttribute(NameOfSong);
-        return null;
+
+    public SearchPage inputNameArtist(String name) {
+        inputNameArtist.sendKeys(name);
+        return this;
     }
 }
