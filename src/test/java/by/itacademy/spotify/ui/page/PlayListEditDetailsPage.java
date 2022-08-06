dc1082378a0a6cb7063d1c5962239051adcd996d
Kirill Vasilenko <41346780+wetanos@users.noreply.github.com>
2022-08-06 23:14:01 +0300

    page: fixed some issues raised after pull review

--- src/test/java/by/itacademy/spotify/ui/page/PlayListEditDetailsPage.java
+++ src/test/java/by/itacademy/spotify/ui/page/PlayListEditDetailsPage.java
@@ -5,18 +5,18 @@ import org.openqa.selenium.support.FindBy;
 
 public class PlayListEditDetailsPage extends PlayListPage {
 
-    @FindBy(xpath="/html/body/div[17]/div/div/div/div[1]/h1")
+    @FindBy(xpath="//*[@data-testid=\"playlist-edit-details-modal\"]/div[1]/h1")
     private WebElement editDetailsTitle;
 
-    @FindBy(xpath = "//*[@id=\"text-input-0a282661aa71b9a3\"]")
-    private WebElement nameField;
+    @FindBy(xpath = "//*[@data-testid=\"playlist-edit-details-name-input\"]")
+    private WebElement nameInput;
 
     @FindBy(xpath = "//*[@data-testid=\"playlist-edit-details-save-button\"]")
     private WebElement saveButton;
 
-    public void editNameFieldValue(String name) {
-        nameField.clear();
-        nameField.sendKeys(name);
+    public void inputNameClearAndType(String name) {
+        nameInput.clear();
+        nameInput.sendKeys(name);
     }
 
     public void clickSaveButton() {
