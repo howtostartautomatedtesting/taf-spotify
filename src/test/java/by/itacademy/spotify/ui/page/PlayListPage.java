7a494b4d041b8711af97f2b461466109b2d1c3d8
trainingby1 <trainingby1@google.com>
2022-08-07 11:19:43 +0300

    refactor more button xpath

--- src/test/java/by/itacademy/spotify/ui/page/PlayListPage.java
+++ src/test/java/by/itacademy/spotify/ui/page/PlayListPage.java
@@ -3,18 +3,18 @@ package by.itacademy.spotify.ui.page;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class PlayListPage extends AuthorizedHomePage{
-    @FindBy(xpath = "//button[@class='T0anrkk_QA4IAQL29get']")
+public class PlayListPage extends AuthorizedHomePage {
+    @FindBy(xpath = "//button[@data-testid='more-button']")
     private WebElement buttonMoreOptionsPlaylist;
     @FindBy(xpath = "//button[@class='wCkmVGEQh3je1hrbsFBY']")
     private WebElement buttonPlaylistName;
 
-    public void clickButtonPlaylistName(){
+    public void clickButtonPlaylistName() {
         buttonPlaylistName.click();
     }
-    public void clickButtonMoreOptionsPlaylist(){
+
+    public void clickButtonMoreOptionsPlaylist() {
         buttonMoreOptionsPlaylist.click();
     }
-
 }
 
