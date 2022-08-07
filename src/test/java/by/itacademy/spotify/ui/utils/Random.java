9d1ec444138df8e2092a4d65c68c61307e9183c8
Alexei Vasilyev <34130226+as-vasilyev@users.noreply.github.com>
2022-08-07 18:49:03 +0300

    feat: Added random string generator

---
+++ src/test/java/by/itacademy/spotify/ui/utils/Random.java
@@ -0,0 +1,11 @@
+package by.itacademy.spotify.ui.utils;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class Random {
+    public String generateText() {
+        int lowerLimit = 8;
+        int upperLimit = 13;
+        return RandomStringUtils.randomPrint(lowerLimit, upperLimit);
+    }
+}
