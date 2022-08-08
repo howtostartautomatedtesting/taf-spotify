f301afe2f05b4952a69b46bdda34099e262ec7ae
Kirill Vasilenko <41346780+wetanos@users.noreply.github.com>
2022-08-08 23:46:46 +0300

    test: deleted imports and exceptions that are not used

--- src/test/java/by/itacademy/spotify/ui/SearchPageTest.java
+++ src/test/java/by/itacademy/spotify/ui/SearchPageTest.java
@@ -2,15 +2,10 @@ package by.itacademy.spotify.ui;
 
 import by.itacademy.spotify.ui.driver.DriverSingleton;
 import by.itacademy.spotify.ui.page.*;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.lang.reflect.InvocationTargetException;
 
 public class SearchPageTest extends BaseTest {
     WebDriver webDriver;
@@ -30,7 +25,7 @@ public class SearchPageTest extends BaseTest {
     }
 
     @Test
-    public void testSearchAndAddSong() throws InvocationTargetException, NullPointerException {
+    public void testSearchAndAddSong() {
         HomePage homePage = new HomePage();
         homePage.clickLogIn();
         LoginPage loginPage = new LoginPage();
