0fc95209b3ce03a793b0a1ae12b10823aaa8623e
trainingby1 <trainingby1@google.com>
2022-08-08 22:51:37 +0300

    refactor util and gradle file

--- src/test/java/by/itacademy/spotify/ui/utils/Random.java
+++ src/test/java/by/itacademy/spotify/ui/utils/Random.java
@@ -3,7 +3,7 @@ package by.itacademy.spotify.ui.utils;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class Random {
-    public String generateText() {
+    public static String generateText() {
         int lowerLimit = 8;
         int upperLimit = 13;
         return RandomStringUtils.randomPrint(lowerLimit, upperLimit);
