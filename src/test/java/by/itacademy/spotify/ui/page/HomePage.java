bb45d0dfbbc219eac055dc257ea03e5ffef86e4f
Anastasiya Karpovich <nastasya.karpovich@gmail.com>
2022-08-08 11:22:03 +0300

    fix the name of locator

--- src/test/java/by/itacademy/spotify/ui/page/HomePage.java
+++ src/test/java/by/itacademy/spotify/ui/page/HomePage.java
@@ -1,16 +1,14 @@
 package by.itacademy.spotify.ui.page;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends BasePage {
     @FindBy(xpath = "//button[@data-testid='login-button']")
-    WebElement buttonLoginIn;
+    WebElement buttonLogIn;
 
     public HomePage clickLoginIn() {
-        buttonLoginIn.click();
+        buttonLogIn.click();
         return this;
     }
 
