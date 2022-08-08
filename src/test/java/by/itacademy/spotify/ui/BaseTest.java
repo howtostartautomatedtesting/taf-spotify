c54511d422bf53a2f84a8bfc380ac99747efd08b
Dmitry_Yakovets <dimak124578@gmail.com>
2022-08-08 23:28:00 +0300

    feat:  UI @Test - Test Login form with empty credentials

--- src/test/java/by/itacademy/spotify/ui/BaseTest.java
+++ src/test/java/by/itacademy/spotify/ui/BaseTest.java
@@ -1,9 +1,22 @@
 package by.itacademy.spotify.ui;
 
 import by.itacademy.spotify.ui.driver.DriverSingleton;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
+    private static final String URL = "https://open.spotify.com/";
+    protected WebDriver webDriver;
+
+    @BeforeMethod
+    public void beforeTest(){
+        webDriver = DriverSingleton.getDriver();
+        webDriver.get(URL);
+        webDriver.manage()
+                .window()
+                .maximize();
+    }
 
     @AfterMethod
     public void finishTest() {
--- src/test/java/by/itacademy/spotify/ui/page/LoginPage.java
+++ src/test/java/by/itacademy/spotify/ui/page/LoginPage.java
@@ -1,30 +1,69 @@
 package by.itacademy.spotify.ui.page;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class LoginPage extends BasePage{
+public class LoginPage extends BasePage {
+    CharSequence[] charSequencesDeleteAll = {Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE};
 
     @FindBy(xpath = "//label[@for='login-username']")
     private WebElement labelUsername;
+
     @FindBy(xpath = "//label[@for='login-password']")
     private WebElement labelPassword;
-    @FindBy(id = "login-username")
+
+    @FindBy(xpath = "//input[@data-testid='login-username']")
     private WebElement inputUsername;
+
     @FindBy(id = "login-password")
     private WebElement inputPassword;
+
     @FindBy(id = "login-button")
     private WebElement buttonLogin;
 
-    public void typeUsername(String username) {
+    @FindBy(xpath = "//div[@data-testid='username-error']/p")
+    private WebElement errorUsername;
+
+    @FindBy(xpath = "//div[@data-testid='password-error']")
+    private WebElement errorPassword;
+
+    public LoginPage typeUsername(String username) {
+        inputUsername.click();
         inputUsername.sendKeys(username);
+        return this;
     }
 
-    public void typePassword(String password) {
+    public LoginPage typePassword(String password) {
+        inputPassword.click();
         inputPassword.sendKeys(password);
+        return this;
     }
 
     public void clickLogin() {
         buttonLogin.click();
+    }
+
+    public LoginPage clearUsername() {
+        inputUsername.sendKeys(charSequencesDeleteAll);
+        return this;
+    }
+
+    public LoginPage clearPassword() {
+        inputPassword.sendKeys(charSequencesDeleteAll);
+        return this;
+    }
+
+    public String getErrorUsernameText() {
+        return errorUsername.getText();
+    }
+
+    public String getErrorUPasswordTest() {
+        return errorPassword.getText();
+    }
+
+    public LoginPage waitForLoginButton() {
+        waitForElementToBeClickable(buttonLogin);
+        return this;
     }
 }
