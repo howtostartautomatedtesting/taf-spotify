82ccfccc307a360f1416e05a52f3e509cd2c1328
k-Anastasiya <100353760+k-Anastasiya@users.noreply.github.com>
2022-08-08 11:37:30 +0300

    Add new Home page (#15)
    * add new home page
    
    * fix method
    
    * fix the name of locator
    
    * fix  names of locator and method
    
    * fix  names of locator and method

---
+++ src/test/java/by/itacademy/spotify/ui/page/HomePage.java
@@ -0,0 +1,19 @@
+package by.itacademy.spotify.ui.page;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage {
+    @FindBy(xpath = "//button[@data-testid='login-button']")
+    private WebElement buttonLogIn;
+
+    public HomePage clickLogIn() {
+        buttonLogIn.click();
+        return this;
+    }
+
+    public HomePage openPage() {
+        driver.get(BASE_URL);
+        return this;
+    }
+}
