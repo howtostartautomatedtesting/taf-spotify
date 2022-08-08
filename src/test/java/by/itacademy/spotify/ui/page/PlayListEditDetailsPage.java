4f86b355247f64c94eee0e5675bcb2632cfb7064
Aliaksandr Zasinets <aliaksandr.zasinets@gmail.com>
2022-08-08 19:30:22 +0300

    fix:names

--- src/test/java/by/itacademy/spotify/ui/page/PlayListEditDetailsPage.java
+++ src/test/java/by/itacademy/spotify/ui/page/PlayListEditDetailsPage.java
@@ -3,12 +3,12 @@ package by.itacademy.spotify.ui.page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
 import java.util.List;
 
 public class PlayListEditDetailsPage extends PlayListPage {
 
     private final By NEW_USER_NAME_PLAYLIST = By.xpath("//div[@class='AINMAUImkAYJd4ertQxy']");
+
     @FindBy(xpath = "//div[@data-testid='playlist-edit-details-modal']/div[1]/h1")
     private WebElement headerEditDetails;
     @FindBy(xpath = "//input[@data-testid='playlist-edit-details-name-input']")
