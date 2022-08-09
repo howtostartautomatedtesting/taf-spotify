c59c4dfd91a9fe34705714158d8e39689732598d
Dmitry Yakovets <dimak124578@gmail.com>
2022-08-09 15:54:51 +0300

    refactor: move befoteTestMethod into test class

--- src/test/java/by/itacademy/spotify/ui/LoginPageTest.java
+++ src/test/java/by/itacademy/spotify/ui/LoginPageTest.java
@@ -1,15 +1,26 @@
 package by.itacademy.spotify.ui;
 
+import by.itacademy.spotify.ui.driver.DriverSingleton;
 import by.itacademy.spotify.ui.page.HomePage;
 import by.itacademy.spotify.ui.page.LoginPage;
 import by.itacademy.spotify.ui.utils.Random;
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LoginPageTest extends BaseTest {
+    private static final String URL = "https://open.spotify.com/";
+    protected WebDriver webDriver;
+
+    @BeforeMethod
+    public void beforeTest() {
+        webDriver = DriverSingleton.getDriver();
+        webDriver.get(URL);
+    }
 
     @Test
     public void testLoginWithEmptyCredentials() {
@@ -20,6 +31,7 @@ public class LoginPageTest extends BaseTest {
         expectedErrorMessageText.add(expectedErrorUsernameText);
         expectedErrorMessageText.add(expectedErrorPasswordText);
         new HomePage().clickLogIn();
+
         LoginPage loginPage = new LoginPage();
         loginPage.waitForLoginButton()
                 .typeUsername(Random.generateText())
@@ -27,6 +39,7 @@ public class LoginPageTest extends BaseTest {
                 .typePassword(Random.generateText())
                 .clearPassword()
                 .clickLogin();
+
         actualErrorMessageText.add(loginPage.getErrorUsernameText());
         actualErrorMessageText.add(loginPage.getErrorUPasswordText());
         Assert.assertEquals(expectedErrorMessageText, actualErrorMessageText);
