19abb12f2bbe0485cce077b3c9dc5b27aecc48b1
Aliaksandr Zasinets <aliaksandr.zasinets@gmail.com>
2022-08-09 09:31:18 +0300

    feat: add waite

--- src/test/java/by/itacademy/spotify/ui/page/LoginPage.java
+++ src/test/java/by/itacademy/spotify/ui/page/LoginPage.java
@@ -21,6 +21,9 @@ public class LoginPage extends BasePage {
 
     @FindBy(id = "login-button")
     private WebElement buttonLogin;
+    @FindBy(xpath = "//button[@data-testid='web-player-link']")
+    private WebElement buttonWebPlayer;
+
 
     @FindBy(xpath = "//div[@data-testid='username-error']/p")
     private WebElement errorUsername;
@@ -38,6 +41,11 @@ public class LoginPage extends BasePage {
         waitForElementToBeClickable(inputPassword);
         inputPassword.sendKeys(password);
         return this;
+    }
+
+    public void clickButtonWebPlayer() {
+        waitForVisibilityOfElement(buttonWebPlayer);
+        buttonWebPlayer.click();
     }
 
     public void clickLogin() {
