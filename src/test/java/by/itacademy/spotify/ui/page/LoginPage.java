fa1d25ff0cf9258e24a8be58e568c3b870c6e492
Aliaksandr Zasinets <aliaksandr.zasinets@gmail.com>
2022-08-09 09:31:18 +0300

    feat: add waite

--- src/test/java/by/itacademy/spotify/ui/page/LoginPage.java
+++ src/test/java/by/itacademy/spotify/ui/page/LoginPage.java
@@ -43,6 +43,11 @@ public class LoginPage extends BasePage {
         return this;
     }
 
+    public void clickButtonWebPlayer() {
+        waitForVisibilityOfElement(buttonWebPlayer);
+        buttonWebPlayer.click();
+    }
+
     public void clickLogin() {
         buttonLogin.click();
     }
