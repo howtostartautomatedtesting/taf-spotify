cc5f53b231b5837d5cb67c596c7689a399cc4019
Anastasiya Karpovich <nastasya.karpovich@gmail.com>
2022-08-09 15:59:12 +0300

    fix  method generateText

--- src/test/java/by/itacademy/spotify/ui/utils/Random.java
+++ src/test/java/by/itacademy/spotify/ui/utils/Random.java
@@ -3,9 +3,7 @@ package by.itacademy.spotify.ui.utils;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class Random {
-    public static String generateText() {
-        int lowerLimit = 8;
-        int upperLimit = 13;
-        return RandomStringUtils.randomPrint(lowerLimit, upperLimit);
+    public static String generateText() {;
+        return RandomStringUtils.randomAlphabetic(10);
     }
 }
