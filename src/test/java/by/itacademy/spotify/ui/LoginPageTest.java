dc60bcdc573ff6d702f4c073d7f89b6d80a06f94
Dmitry Yakovets <dimak124578@gmail.com>
2022-08-10 14:44:38 +0300

    refactor testLoginWithEmptyCredentials method

--- src/test/java/by/itacademy/spotify/ui/LoginPageTest.java
+++ src/test/java/by/itacademy/spotify/ui/LoginPageTest.java
@@ -6,31 +6,26 @@ import by.itacademy.spotify.ui.utils.Random;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class LoginPageTest extends BaseTest {
 
     @Test
     public void testLoginWithEmptyCredentials() {
-        List<String> expectedErrorMessageText = new ArrayList<>();
-        List<String> actualErrorMessageText = new ArrayList<>();
+        // GIVEN
         String expectedErrorUsernameText = "Please enter your Spotify username or email address.";
         String expectedErrorPasswordText = "Please enter your password.";
-        expectedErrorMessageText.add(expectedErrorUsernameText);
-        expectedErrorMessageText.add(expectedErrorPasswordText);
-        new HomePage().openPage().clickLogIn();
+        new HomePage().openPage()
+                .clickLogIn();
 
+        // WHEN
         LoginPage loginPage = new LoginPage();
-        loginPage.waitForLoginButton()
-                .typeUsername(Random.generateText())
+        loginPage.typeUsername(Random.generateText())
                 .clearUsername()
                 .typePassword(Random.generateText())
                 .clearPassword()
                 .clickLogin();
 
-        actualErrorMessageText.add(loginPage.getErrorUsernameText());
-        actualErrorMessageText.add(loginPage.getErrorUPasswordText());
-        Assert.assertEquals(expectedErrorMessageText, actualErrorMessageText);
+        // THEN
+        Assert.assertEquals(expectedErrorUsernameText, loginPage.getErrorUsernameText());
+        Assert.assertEquals(expectedErrorPasswordText, loginPage.getErrorUPasswordText());
     }
 }
