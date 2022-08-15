febfa853b69a819f5f1bb44ab0a5c7d329ec6cb7
Kirill Vasilenko <41346780+wetanos@users.noreply.github.com>
2022-08-15 23:39:51 +0300

    refactor: rebase

--- src/test/java/by/itacademy/spotify/ui/utils/Randomizer.java
+++ src/test/java/by/itacademy/spotify/ui/utils/Randomizer.java
@@ -1,17 +1,24 @@
 package by.itacademy.spotify.ui.utils;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class ForbiddenCharacters {
+public class Randomizer {
+    public static String generateText() {
+        int lowerLimit = 8;
+        int upperLimit = 13;
+        return RandomStringUtils.randomAlphabetic(lowerLimit, upperLimit);
+    }
     public static String getForbiddenCharacter() {
         List<String> forbiddenCharacters = new ArrayList<>();
         forbiddenCharacters.add("'");
         forbiddenCharacters.add("`");
         forbiddenCharacters.add("&");
         forbiddenCharacters.add(":");
-        int randomIndex = new Random().nextInt(forbiddenCharacters.size());
+        int randomIndex = new java.util.Random().nextInt(forbiddenCharacters.size());
         return forbiddenCharacters.get(randomIndex);
     }
 }
