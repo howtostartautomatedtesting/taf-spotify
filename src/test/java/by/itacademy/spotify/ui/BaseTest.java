b90d80d7519d3e9f09a91127b0c63953dd502163
trainingby1 <trainingby1@google.com>
2022-08-18 12:39:23 +0300

    refactor: play list edit details page

--- src/test/java/by/itacademy/spotify/ui/BaseTest.java
+++ src/test/java/by/itacademy/spotify/ui/BaseTest.java
@@ -1,17 +1,9 @@
 package by.itacademy.spotify.ui;
 
 import by.itacademy.spotify.ui.driver.DriverSingleton;
-import by.itacademy.spotify.ui.steps.AuthorizedStep;
-import by.itacademy.spotify.ui.utils.UserCreator;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
 
 public class BaseTest {
-
-
-    @BeforeGroups("edit-play-list")
-    public void logIn(){
-        AuthorizedStep.logIn(UserCreator.createDefaultUser());
-    }
 
     @AfterMethod
     public void finishTest() {
