7d8d7de68df4b04d974db3d635aa6d7b67ce23c3
trainingby1 <trainingby1@google.com>
2022-08-18 12:39:23 +0300

    refactor: play list edit details page

--- src/test/java/by/itacademy/spotify/ui/PlayListEditDetailsPageTest.java
+++ src/test/java/by/itacademy/spotify/ui/PlayListEditDetailsPageTest.java
@@ -2,7 +2,9 @@ package by.itacademy.spotify.ui;
 
 import by.itacademy.spotify.ui.page.PlayListEditDetailsPage;
 import by.itacademy.spotify.ui.page.PlayListPage;
+import by.itacademy.spotify.ui.steps.AuthorizedStep;
 import by.itacademy.spotify.ui.utils.Random;
+import by.itacademy.spotify.ui.utils.UserCreator;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,13 +12,26 @@ public class PlayListEditDetailsPageTest extends BaseTest {
     PlayListPage playListPage;
     PlayListEditDetailsPage playListEditDetailsPage;
 
-    @Test(groups = "edit-play-list")
-    public void testEditDetailsWithCorrectData() {
+    @Test
+    public void testEditDetailsUI() {
+        AuthorizedStep.logIn(UserCreator.createDefaultUser());
         String userInputNamePlayList = Random.generateText();
         String userInputDescriptionPlayList = Random.generateText();
+        playListPage = new PlayListPage()
+                .clickButtonCreatePlayList();
 
-        //
-        //добавление плейлиста
+        playListEditDetailsPage = new PlayListEditDetailsPage()
+                .clickButtonCookieClose()
+                .clickButtonDefaultPlayList();
+
+        Assert.assertTrue(playListEditDetailsPage.isDisplayedHeaderDetails());
+    }
+
+    @Test
+    public void testEditDetailsWithCorrectData() {
+        AuthorizedStep.logIn(UserCreator.createDefaultUser());
+        String userInputNamePlayList = Random.generateText();
+        String userInputDescriptionPlayList = Random.generateText();
         playListPage = new PlayListPage()
                 .clickButtonCreatePlayList();
 
@@ -24,18 +39,7 @@ public class PlayListEditDetailsPageTest extends BaseTest {
                 .clickButtonCookieClose()
                 .clickButtonDefaultPlayList()
                 .typeName(userInputNamePlayList)
-                .typeDescription(userInputDescriptionPlayList);
-
-        // я не знаю для чего делать проверку дальнейших 3 ассертов. т.к.
-        // нам нужно было проверить изменилось ли название плейлиста, а это последний ассерт.
-        // Если же clickSave() поднять выше перед ассертами, то тогда когда выполнится это действие и закроется форма,
-        // то ассерты с названием в данной форме высвечивающейся уже не пройдут проверку
-
-        Assert.assertTrue(playListEditDetailsPage.isDisplayedHeaderDetails());
-        Assert.assertEquals(userInputNamePlayList, playListEditDetailsPage.getUserInputNamePlayList());
-        Assert.assertEquals(userInputDescriptionPlayList, playListEditDetailsPage.getUserInputDescriptionPlayList());
-
-        playListEditDetailsPage
+                .typeDescription(userInputDescriptionPlayList)
                 .clickSave();
 
         Assert.assertEquals(userInputNamePlayList, playListEditDetailsPage.getNewUserNamePlayList(userInputNamePlayList));
