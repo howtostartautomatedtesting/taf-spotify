13eab4b1afb11656e1f6f52d1230ea3d6f5759d7
trainingby1 <trainingby1@google.com>
2022-09-05 15:08:54 +0300

    refactor: credentials

--- src/test/java/by/itacademy/spotify/ui/utils/UserCreator.java
+++ src/test/java/by/itacademy/spotify/ui/utils/UserCreator.java
@@ -3,7 +3,7 @@ package by.itacademy.spotify.ui.utils;
 import by.itacademy.spotify.ui.entity.User;
 
 public class UserCreator {
-    public static final String USER_NAME = "howtostartautomatedtesting@gmail.comm";
+    public static final String USER_NAME = "howtostartautomatedtesting@gmail.com";
     public static final String USER_PASSWORD = "h0W-2-sTArt-at";
 
     public static User createDefaultUser(){
